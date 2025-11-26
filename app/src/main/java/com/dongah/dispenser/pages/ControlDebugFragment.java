84a946186b20c6eae5e43b1836f48a6c4e26054e
ghchoi-dongah <ghchoi@dongahelecomm.co.kr>
2025-11-26 16:02:21 +0900

    control baord 나가기 버튼 기능 추가

--- app/src/main/java/com/dongah/dispenser/pages/ControlDebugFragment.java
+++ app/src/main/java/com/dongah/dispenser/pages/ControlDebugFragment.java
@@ -7,24 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.dongah.dispenser.MainActivity;
 import com.dongah.dispenser.R;
+import com.dongah.dispenser.basefunction.UiSeq;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ControlDebugFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ControlDebugFragment extends Fragment {
+public class ControlDebugFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String CHANNEL = "CHANNEL";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int mChannel;
+
+    Button btnClose;
 
     public ControlDebugFragment() {
         // Required empty public constructor
@@ -54,13 +64,24 @@ public class ControlDebugFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            mChannel = getArguments().getInt(CHANNEL);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control_debug, container, false);
+        View view = inflater.inflate(R.layout.fragment_control_debug, container, false);
+        btnClose = view.findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int getId = v.getId();
+        if (Objects.equals(getId, R.id.btnClose)) {
+            ((MainActivity) MainActivity.mContext).getFragmentChange().onFragmentChange(mChannel, UiSeq.ENVIRONMENT, "ENVIRONMENT", null);
+        }
     }
 }
