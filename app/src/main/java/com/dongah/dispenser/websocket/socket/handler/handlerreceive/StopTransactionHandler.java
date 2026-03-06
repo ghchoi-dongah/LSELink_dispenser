9cfb9df1f7025b765ff300cd9caaf73d41972910
ghchoi@dongahelecomm.co.kr <ghchoi@dongahelecomm.co.kr>
2026-03-06 14:54:15 +0900

    StopTransaction Accepted 처리 수정

--- app/src/main/java/com/dongah/dispenser/websocket/socket/handler/handlerreceive/StopTransactionHandler.java
+++ app/src/main/java/com/dongah/dispenser/websocket/socket/handler/handlerreceive/StopTransactionHandler.java
@@ -1,6 +1,8 @@
 package com.dongah.dispenser.websocket.socket.handler.handlerreceive;
 
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.annotation.RequiresApi;
 
@@ -8,6 +10,7 @@ import com.dongah.dispenser.MainActivity;
 import com.dongah.dispenser.basefunction.ChargingCurrentData;
 import com.dongah.dispenser.websocket.ocpp.core.AuthorizationStatus;
 import com.dongah.dispenser.websocket.ocpp.core.ChargePointStatus;
+import com.dongah.dispenser.websocket.ocpp.core.Reason;
 import com.dongah.dispenser.websocket.socket.OcppHandler;
 import com.dongah.dispenser.websocket.socket.handler.handlersend.ChargingAlarmReq;
 import com.dongah.dispenser.websocket.socket.handler.handlersend.StatusNotificationReq;
@@ -23,29 +26,31 @@ public class StopTransactionHandler implements OcppHandler  {
     public void handle(JSONObject payload, int connectorId, String messageId) throws Exception {
         MainActivity activity = (MainActivity) MainActivity.mContext;
         ChargingCurrentData chargingCurrentData = activity.getChargingCurrentData(connectorId-1);
+        final StatusNotificationReq statusNotificationReq;
 
         JSONObject idTagInfo = payload.getJSONObject("idTagInfo");
 
         AuthorizationStatus status = AuthorizationStatus.valueOf(idTagInfo.getString("status"));
         String parentIdTag = idTagInfo.has("parentIdTag") ? idTagInfo.getString("parentIdTag") : null;
 
-
-        // DataTransfer ChargingAlarm
-        ChargingAlarmReq chargingAlarmReq = new ChargingAlarmReq(connectorId);
-        chargingAlarmReq.sendChargingAlarmReq(3);
-
         //accept continue
-//        if (Objects.equals(status, AuthorizationStatus.Accepted)) {
-//            chargingCurrentData.setChargePointStatus(ChargePointStatus.Available);
-//        }
+        if (Objects.equals(status, AuthorizationStatus.Accepted)) {
+            statusNotificationReq = new StatusNotificationReq(connectorId);
+            chargingCurrentData.setChargePointStatus(ChargePointStatus.Finishing);
+            statusNotificationReq.sendStatusNotification(connectorId, ChargePointStatus.Finishing);
 
+            // DataTransfer ChargingAlarm
+            ChargingAlarmReq chargingAlarmReq = new ChargingAlarmReq(connectorId);
+            chargingAlarmReq.sendChargingAlarmReq(3);
 
-        chargingCurrentData.setChargePointStatus(ChargePointStatus.Available);
-        StatusNotificationReq statusNotificationReq = new StatusNotificationReq(chargingCurrentData.getConnectorId());
-        statusNotificationReq.sendStatusNotification();
-
-//        activity.getClassUiProcess(connectorId-1).setUiSeq(UiSeq.FINISH);
-//        FragmentChange fragmentChange = new FragmentChange();
-//        fragmentChange.onFragmentChange(connectorId-1, UiSeq.FINISH, "FINISH", null);
+            // EVDisconnected 인 경우
+            if (Objects.equals(chargingCurrentData.getStopReason(), Reason.EVDisconnected)) {
+                Handler mainHandler = new Handler(Looper.getMainLooper());
+                mainHandler.postDelayed(() -> {
+                    chargingCurrentData.setChargePointStatus(ChargePointStatus.Available);
+                    statusNotificationReq.sendStatusNotification(connectorId, ChargePointStatus.Available);
+                }, 3000);
+            }
+        }
     }
 }
