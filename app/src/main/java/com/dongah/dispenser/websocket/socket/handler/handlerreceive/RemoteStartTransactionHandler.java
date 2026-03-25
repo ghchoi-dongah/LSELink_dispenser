7e6769d0d481e86fa7f40e564edc8dee0161aad3
ghchoi@dongahelecomm.co.kr <ghchoi@dongahelecomm.co.kr>
2026-03-25 12:07:35 +0900

    RemoteStartTransaction 응답 처리 보완

--- app/src/main/java/com/dongah/dispenser/websocket/socket/handler/handlerreceive/RemoteStartTransactionHandler.java
+++ app/src/main/java/com/dongah/dispenser/websocket/socket/handler/handlerreceive/RemoteStartTransactionHandler.java
@@ -8,9 +8,12 @@ import com.dongah.dispenser.MainActivity;
 import com.dongah.dispenser.basefunction.ChargingCurrentData;
 import com.dongah.dispenser.basefunction.PaymentType;
 import com.dongah.dispenser.basefunction.UiSeq;
+import com.dongah.dispenser.websocket.ocpp.core.ChargePointStatus;
 import com.dongah.dispenser.websocket.ocpp.core.RemoteStartStopStatus;
 import com.dongah.dispenser.websocket.ocpp.core.RemoteStartTransactionConfirmation;
 import com.dongah.dispenser.websocket.socket.OcppHandler;
+import com.dongah.dispenser.websocket.socket.handler.handlersend.AuthorizeReq;
+import com.dongah.dispenser.websocket.socket.handler.handlersend.StatusNotificationReq;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -42,6 +45,7 @@ public class RemoteStartTransactionHandler implements OcppHandler  {
         try {
             MainActivity activity = ((MainActivity) MainActivity.mContext);
             UiSeq uiSeq = activity.getClassUiProcess(connectorId-1).getUiSeq();
+            ChargingCurrentData chargingCurrentData = activity.getChargingCurrentData(connectorId-1);
 
             RemoteStartStopStatus status = !Objects.equals(uiSeq, UiSeq.INIT) ? RemoteStartStopStatus.Rejected
                     : connectorId == 0 ? RemoteStartStopStatus.Rejected : RemoteStartStopStatus.Accepted;
@@ -52,6 +56,19 @@ public class RemoteStartTransactionHandler implements OcppHandler  {
                     messageId,
                     remoteStartTransactionConfirmation
             );
+
+            if (Objects.equals(status, RemoteStartStopStatus.Accepted)) {
+                chargingCurrentData.setAuthType("C");
+
+                // Authorize
+                AuthorizeReq authorizeReq = new AuthorizeReq(connectorId);
+                authorizeReq.sendAuthorize(chargingCurrentData.getIdTag());
+
+                // StatusNotification
+                chargingCurrentData.setChargePointStatus(ChargePointStatus.Preparing);
+                StatusNotificationReq statusNotificationReq = new StatusNotificationReq(connectorId);
+                statusNotificationReq.sendStatusNotification();
+            }
         } catch (Exception e) {
             logger.error(" sendResponse error : {}", e.getMessage());
         }
